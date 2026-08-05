84dd54d6587f45d6d5f5b045284b9a62e9f90a6e
Vandan Bhangale <vandan.bhangale@rubamin.com>
2026-08-05 12:42:55 +0530

    fix: Cookie issue

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -7,8 +7,9 @@ const postLogin = async (req,res) => {
 
         res.cookie('token',result.token,{
             httpOnly:true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite:'strict',
+            // secure: process.env.NODE_ENV === 'production',
+            secure: false,
+            sameSite: 'lax',
             maxAge: 8 * 60 * 60 * 1000 //8 hours
         })
 
